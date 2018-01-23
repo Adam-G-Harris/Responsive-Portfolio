c60c9fc268e9bb2668724e13e23e946c1f5ceb9b
Adam Harris <adamgrantharris14@gmail.com>
2018-01-23 14:15:06 -0600

    1.0.2 { General ~ Added background canvas to play with | Canvas calc W/H on resize }

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 window.onload = () => {
 
 	AOS.init();
+	resize();
+
+	window.addEventListener('resize', resize, false);
+
+	function resize() {
+		let canvas = document.getElementById('canvas');
+		let ctx = canvas.getContext('2d');
+		canvas.width = document.body.clientWidth;
+		canvas.height = document.body.clientHeight;
+		canvas.style.width = document.body.clientWidth;
+		canvas.style.height = document.body.clientHeight;
+	}
 
 };
